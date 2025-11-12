e260310c02c4ef2671185d2d63bd93ce4830becd
Noah Horton <noah@unsupervised.com>
2025-11-12 16:01:37 -0700

    Set stdout to auto-flush for logging
    Enable auto-flushing of stdout for logging during job execution.

--- lib/active_job_kubernetes/tasks.rb
+++ lib/active_job_kubernetes/tasks.rb
@@ -4,6 +4,9 @@ namespace :active_job_kubernetes do
   task run_job: :environment do
     # We expect to have the classes all pre-loaded, so forcing that
     Rails.application.eager_load!
+    # Set stdout to auto-flush so log messages come out while we run
+    STDOUT.sync = true
+
     ActiveJob::Base.execute(JSON.parse(ENV['SERIALIZED_JOB']))
   end
 end
